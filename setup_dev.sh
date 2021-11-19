b3c33cebb7d94eef7f9670966d6c8faca30f4728
Jan Vollmer <51451323+kreyoo@users.noreply.github.com>
2021-11-19 12:48:39 +0100

    Update setup_dev.sh

--- setup_dev.sh
+++ setup_dev.sh
@@ -1,5 +1,8 @@
-sudo pacman -S uwsgi python python-pip fabric npm nodejs python-poetry
+sudo pacman -S uwsgi python python-pip fabric npm nodejs python-venv
+python -m venv .venv
+. .venv/bin/activate
 poetry config virtualenvs.create true
 poetry config virtualenvs.in-project true
 poetry install
 fab setup_dev
+deactivate
