9a9c7254530ace612075b857409f1b85d44d61ec
kreyoo <zunder325@gmail.com>
2021-12-23 22:04:20 +0100

    switch to yarn

--- setup_dev.sh
+++ setup_dev.sh
@@ -1,6 +1,7 @@
-sudo pacman -S uwsgi python python-pip fabric npm nodejs python-venv
+sudo pacman -S uwsgi python python-pip fabric yarn nodejs python-venv redis
 python -m venv .venv
 . .venv/bin/activate
+pip install poetry
 poetry config virtualenvs.create true
 poetry config virtualenvs.in-project true
 poetry install
@@ -9,3 +10,6 @@ npm i .
 npm audit fix
 cd ..
 deactivate
+#curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+#echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+#sudo apt-get update && sudo apt-get install yarn
