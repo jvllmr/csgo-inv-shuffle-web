aeebe58b6bd160a16203cdb2dfcf35d50224765c
Jan Vollmer <51451323+kreyoo@users.noreply.github.com>
2022-01-17 09:01:01 +0100

    steam has multiple cdn providers

--- tests/test_routes/test_profile.py
+++ tests/test_routes/test_profile.py
@@ -5,13 +5,19 @@ def test_get_profile_picture(authed_client: FlaskClient):
 
     resp = authed_client.get("/profile_picture")
     assert resp.status_code == 200
-    assert resp.get_json()["link"].startswith(
+    link = resp.get_json()["link"]
+    assert link.startswith(
         "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars"
+    ) or link.startswith(
+        "https://cdn.akamai.steamstatic.com/steamcommunity/public/images/avatars"
     )
     cached_resp = authed_client.get("/profile_picture")
     assert cached_resp.status_code == 200
-    assert cached_resp.get_json()["link"].startswith(
+    cached_link = cached_resp.get_json()["link"]
+    assert cached_link.startswith(
         "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars"
+    ) or cached_link.startswith(
+        "https://cdn.akamai.steamstatic.com/steamcommunity/public/images/avatars"
     )
 
 
