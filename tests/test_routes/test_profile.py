8a690a4bbad71407acc949c49ab225f9f46d28d8
kreyoo <zunder325@gmail.com>
2022-03-12 21:52:24 +0100

    add new steam avatar link to tests

--- tests/test_routes/test_profile.py
+++ tests/test_routes/test_profile.py
@@ -6,19 +6,27 @@ def test_get_profile_picture(authed_client: FlaskClient):
     resp = authed_client.get("/profile_picture")
     assert resp.status_code == 200
     link = resp.get_json()["link"]
-    assert link.startswith(
-        "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars"
-    ) or link.startswith(
-        "https://cdn.akamai.steamstatic.com/steamcommunity/public/images/avatars"
-    )
+    assert (
+        link.startswith(
+            "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars"
+        )
+        or link.startswith(
+            "https://cdn.akamai.steamstatic.com/steamcommunity/public/images/avatars"
+        )
+        or link.startswith("https://avatars.cloudflare.steamstatic.com")
+    ), link
     cached_resp = authed_client.get("/profile_picture")
     assert cached_resp.status_code == 200
     cached_link = cached_resp.get_json()["link"]
-    assert cached_link.startswith(
-        "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars"
-    ) or cached_link.startswith(
-        "https://cdn.akamai.steamstatic.com/steamcommunity/public/images/avatars"
-    )
+    assert (
+        cached_link.startswith(
+            "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars"
+        )
+        or cached_link.startswith(
+            "https://cdn.akamai.steamstatic.com/steamcommunity/public/images/avatars"
+        )
+        or link.startswith("https://avatars.cloudflare.steamstatic.com")
+    ), cached_link
 
 
 def test_get_inv(authed_client: FlaskClient):
