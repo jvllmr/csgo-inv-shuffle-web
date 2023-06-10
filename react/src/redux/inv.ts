a766cab4c60262bac0f0de80f2e0fa5565a100c0
Jan Vollmer <jan@vllmr.dev>
2023-06-10 16:28:00 +0200

    fix file formatting

--- react/src/redux/inv.ts
+++ react/src/redux/inv.ts
@@ -3,60 +3,60 @@ import { Item } from "../components/Item";
 import { RootState } from "../redux";
 const invDBReadyEvent = new Event("invDBReady");
 interface InitialState {
-	inv: Item[];
-	invDBReady: boolean;
+  inv: Item[];
+  invDBReady: boolean;
 }
 
 let db: IDBDatabase;
 const OpenRequest = indexedDB.open("inv", 2);
 OpenRequest.onupgradeneeded = () => {
-	db = OpenRequest.result;
-	db.createObjectStore("inv");
+  db = OpenRequest.result;
+  db.createObjectStore("inv");
 };
 
 OpenRequest.onsuccess = () => {
-	db = OpenRequest.result;
-	document.dispatchEvent(invDBReadyEvent);
+  db = OpenRequest.result;
+  document.dispatchEvent(invDBReadyEvent);
 };
 
 export async function DBgetInv(): Promise<Item[]> {
-	return new Promise<Item[]>((resolve, reject) => {
-		const transaction = db.transaction("inv");
-		const objStore = transaction.objectStore("inv");
-		const getter = objStore.getAll();
-		getter.onsuccess = () => resolve(getter.result);
-	});
+  return new Promise<Item[]>((resolve, reject) => {
+    const transaction = db.transaction("inv");
+    const objStore = transaction.objectStore("inv");
+    const getter = objStore.getAll();
+    getter.onsuccess = () => resolve(getter.result);
+  });
 }
 
 function saveToDB(objStore: string, new_val: Item[]) {
-	if (!db) return;
-	const transaction = db.transaction(objStore, "readwrite");
-	new_val = JSON.parse(JSON.stringify(new_val));
-	const mapStore = transaction.objectStore(objStore);
-	const clearing = mapStore.clear();
-	clearing.onsuccess = () => {
-		const putters: IDBRequest<IDBValidKey>[] = [];
-		for (let index = 0; index < new_val.length; index++) {
-			putters.push(mapStore.put(new_val[index], index));
-			putters[index].onsuccess = () => {};
-		}
-	};
+  if (!db) return;
+  const transaction = db.transaction(objStore, "readwrite");
+  new_val = JSON.parse(JSON.stringify(new_val));
+  const mapStore = transaction.objectStore(objStore);
+  const clearing = mapStore.clear();
+  clearing.onsuccess = () => {
+    const putters: IDBRequest<IDBValidKey>[] = [];
+    for (let index = 0; index < new_val.length; index++) {
+      putters.push(mapStore.put(new_val[index], index));
+      putters[index].onsuccess = () => {};
+    }
+  };
 }
 
 const initialState: InitialState = { inv: [], invDBReady: false };
 
 export const invSlice = createSlice({
-	name: "inv",
-	initialState: initialState,
-	reducers: {
-		setInv: (state, action: PayloadAction<Item[]>) => {
-			state.inv = [...action.payload];
-			saveToDB("inv", [...action.payload]);
-		},
-		setInvDBReady: (state) => {
-			state.invDBReady = true;
-		},
-	},
+  name: "inv",
+  initialState: initialState,
+  reducers: {
+    setInv: (state, action: PayloadAction<Item[]>) => {
+      state.inv = [...action.payload];
+      saveToDB("inv", [...action.payload]);
+    },
+    setInvDBReady: (state) => {
+      state.invDBReady = true;
+    },
+  },
 });
 
 export const { setInv, setInvDBReady } = invSlice.actions;
--- react/src/utils/inventory.ts
+++ react/src/utils/inventory.ts
@@ -2,39 +2,39 @@ import { Item } from "../components/Item";
 import store from "../redux";
 
 export function getItem(id: string | number): Item | undefined {
-	id = String(id);
-	for (const item of store.getState().inv.inv) {
-		if (item.id === id) return item;
-	}
+  id = String(id);
+  for (const item of store.getState().inv.inv) {
+    if (item.id === id) return item;
+  }
 }
 
 export function hasItem(item: Item | string, items: Item[]): boolean {
-	const item_ids = items.map((_item: Item) => _item.id);
-	if (typeof item !== "string") item = item.id;
-	return item_ids.includes(item);
+  const item_ids = items.map((_item: Item) => _item.id);
+  if (typeof item !== "string") item = item.id;
+  return item_ids.includes(item);
 }
 
 export function hasIntersectingSlots(
-	item: Item | string,
-	items: Item[]
+  item: Item | string,
+  items: Item[]
 ): boolean {
-	if (typeof item === "string") item = getItem(item)!;
-	const items_slots = items.map((_item: Item) =>
-		_item.shuffle_slots.concat(
-			..._item.shuffle_slots_ct,
-			..._item.shuffle_slots_t
-		)
-	);
-	const item_slots = item.shuffle_slots.concat(
-		...item.shuffle_slots_ct,
-		...item.shuffle_slots_t
-	);
+  if (typeof item === "string") item = getItem(item)!;
+  const items_slots = items.map((_item: Item) =>
+    _item.shuffle_slots.concat(
+      ..._item.shuffle_slots_ct,
+      ..._item.shuffle_slots_t
+    )
+  );
+  const item_slots = item.shuffle_slots.concat(
+    ...item.shuffle_slots_ct,
+    ...item.shuffle_slots_t
+  );
 
-	for (const _item of items_slots) {
-		for (const n1 of _item) {
-			if (item_slots.includes(n1)) return true;
-		}
-	}
+  for (const _item of items_slots) {
+    for (const n1 of _item) {
+      if (item_slots.includes(n1)) return true;
+    }
+  }
 
-	return false;
+  return false;
 }
